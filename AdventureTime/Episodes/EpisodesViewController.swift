e3c311e548a1bd82a480b210bd853f8e58f8f33b
Bruno <brunomilliet89@gmail.com>
2019-05-04 17:11:13 -0300

    fixing url params

---
+++ AdventureTime/Episodes/EpisodesViewController.swift
@@ -0,0 +1,10 @@
+import UIKit
+
+class EpisodesViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+}
